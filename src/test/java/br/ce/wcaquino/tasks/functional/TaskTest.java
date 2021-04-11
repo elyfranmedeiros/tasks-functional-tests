3a274ffb71298fb50bbeefd3a8966068ec578651
Elyfran Medeiros <elyfran.medeiros@edirectinsure.com>
2021-04-11 11:16:13 -0300

    Initial version

---
+++ src/test/java/br/ce/wcaquino/tasks/functional/TaskTest.java
@@ -0,0 +1,77 @@
+package br.ce.wcaquino.tasks.functional;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TaskTest {
+
+	private WebDriver driver;
+	
+	public WebDriver acessarAplicacao() {
+		driver = new ChromeDriver();
+		driver.get("http://localhost:8001/tasks/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+	
+	@Test
+	public void testUserCreatedSuccessfully() throws InterruptedException {
+		WebDriver driver = acessarAplicacao();
+		try {
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Task Name");
+			driver.findElement(By.id("dueDate")).sendKeys("25/12/2030");
+			driver.findElement(By.id("saveButton")).click();
+			assertEquals("Success!", driver.findElement(By.id("message")).getText());
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void testShouldNotAddOldDate() throws InterruptedException {
+		WebDriver driver = acessarAplicacao();
+		try {
+		driver.findElement(By.id("addTodo")).click();
+		driver.findElement(By.id("task")).sendKeys("Task Name");
+		driver.findElement(By.id("dueDate")).sendKeys("25/12/2010");
+		driver.findElement(By.id("saveButton")).click();
+		assertEquals("Due date must not be in past", driver.findElement(By.id("message")).getText());
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void testShouldNotAcceptWithoutDescription() throws InterruptedException {
+		WebDriver driver = acessarAplicacao();
+		try {
+		driver.findElement(By.id("addTodo")).click();
+		driver.findElement(By.id("dueDate")).sendKeys("25/12/2030");
+		driver.findElement(By.id("saveButton")).click();
+		assertEquals("Fill the task description", driver.findElement(By.id("message")).getText());
+		} finally {
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void testShouldNotAcceptWithoutDate() throws InterruptedException {
+		WebDriver driver = acessarAplicacao();
+		try {
+		driver.findElement(By.id("addTodo")).click();
+		driver.findElement(By.id("task")).sendKeys("Task Name");
+		driver.findElement(By.id("saveButton")).click();
+		assertEquals("Fill the due date", driver.findElement(By.id("message")).getText());
+		} finally {
+			driver.quit();
+		}
+	}
+}
