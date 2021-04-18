0a48326a8e7959bdd1516c9a94a26c5866a556b8
Elyfran Medeiros <elyfran.medeiros@edirectinsure.com>
2021-04-18 14:44:20 -0300

    Adding remove

--- src/test/java/br/ce/wcaquino/tasks/functional/TaskTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TaskTest.java
@@ -16,9 +16,9 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class TaskTest {
 
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-		WebDriver driver = new ChromeDriver();
-		//DesiredCapabilities cap = DesiredCapabilities.chrome();
-		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.11:4444/wd/hub"), cap);
+		//WebDriver driver = new ChromeDriver();
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.11:4444/wd/hub"), cap);
 		driver.get("http://192.168.0.11:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
